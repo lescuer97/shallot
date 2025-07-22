0373a0083fa01ebc6cf79ccbad4b24d8ffada445
leonardo <leoescuer@protonmail.com>
2025-07-22 21:31:03 +0200

    add nostr client

--- cmd/shallot/main.go
+++ cmd/shallot/main.go
@@ -30,8 +30,23 @@ func main() {
 	store := make(map[string]*nostr.Event, 120)
 	relay.StoreEvent = append(relay.StoreEvent,
 		func(ctx context.Context, event *nostr.Event) error {
+			log.Printf("nostEvent. %+v", event)
 			store[event.ID] = event
 			return nil
+		},
+	)
+	relay.QueryEvents = append(relay.QueryEvents,
+		func(ctx context.Context, filter nostr.Filter) (chan *nostr.Event, error) {
+			ch := make(chan *nostr.Event)
+			go func() {
+				for _, evt := range store {
+					if filter.Matches(evt) {
+						ch <- evt
+					}
+				}
+				close(ch)
+			}()
+			return ch, nil
 		},
 	)
 
@@ -42,14 +57,18 @@ func main() {
 	// 3. Relaying the message to the next peer.
 
 	// mux := relay.Router()
-	// set up other http handlers
+	// // set up other http handlers
 	// mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 	// 	w.Header().Set("content-type", "text/html")
 	// 	fmt.Fprintf(w, `<b>welcome</b> to my relay!`)
 	// })
 
 	// start the server
-	fmt.Println("running on :3334")
-	http.ListenAndServe(":3334", relay)
+	fmt.Println("running on :10023")
+	err = http.ListenAndServe(":10023", relay)
+	if err != nil {
+		log.Panicf("could not listen to server. %w", err)
+
+	}
 
 }
